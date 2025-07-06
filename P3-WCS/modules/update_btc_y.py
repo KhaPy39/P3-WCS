f9d692611b017f5fd1cd8264af3a9bcc060fdd45
KhaPy39 <k.dahmani@live.fr>
2025-07-06 17:01:06 +0200

    Update update_btc_y.py

--- P3-WCS/modules/update_btc_y.py
+++ P3-WCS/modules/update_btc_y.py
@@ -1,10 +1,11 @@
 from supabase_client import get_supabase_connection
 from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 
 def main():
     supabase = get_supabase_connection()
 
-    now = datetime.now()
+    now = datetime.now(ZoneInfo("Europe/Paris"))
     segment_start = now.replace(month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
     segment_end = segment_start.replace(year=segment_start.year + 1)
 
